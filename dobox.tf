c3f36bb50b6cc68c66b4da71b1c893b8a336176f
Eris Fairbanks <erisfairbanks@gmail.com>
2021-03-10 15:04:11 -0500

    Putting my website back up. Still need to rename the instance in Terraform to be something other than "streaming." Should set up subdomains for particular boxes and conditional deployment based on tfvars.

--- dobox.tf
+++ dobox.tf
@@ -7,6 +7,39 @@ resource "digitalocean_droplet" "stream" {
   image = "ubuntu-18-04-x64"
   name = "dobox"
   region = "nyc1"
+  size = "s-1vcpu-1gb"
+  private_networking = true
+  ssh_keys = [
+    data.digitalocean_ssh_key.terraform.id
+  ]
+  connection {
+    host = self.ipv4_address
+    user = "root"
+    type = "ssh"
+    private_key = file(var.pvt_key)
+    timeout = "2m"
+  }
+  provisioner "remote-exec" {
+    inline = [
+      "export PATH=$PATH:/usr/bin",
+      "sudo add-apt-repository universe",
+      "sudo apt-get update",
+      "sudo apt install -y build-essential apache2 ruby ruby-dev ruby-bundler git",
+      "sudo ufw allow 'Apache'",
+      "git clone https://github.com/efairbanks/ericfairbanks-org-jekyll-bootstrap.git",
+      "cd ericfairbanks-org-jekyll-bootstrap",
+      "gem install jekyll ffi",
+      "bundle install",
+      "bundle exec jekyll build -d /var/www/html/",
+    ]
+  }
+}
+
+/*
+resource "digitalocean_droplet" "stream" {
+  image = "ubuntu-18-04-x64"
+  name = "dobox"
+  region = "nyc1"
   size = "s-2vcpu-2gb"
   private_networking = true
   ssh_keys = [
@@ -37,3 +70,4 @@ resource "digitalocean_droplet" "stream" {
     ]
   }
 }
+*/
