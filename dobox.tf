6285e72cbfc21571151c058cf5850b01dff21a1b
Eris Fairbanks <erisfairbanks@gmail.com>
2021-04-03 15:25:07 -0400

    tweaks

--- dobox.tf
+++ dobox.tf
@@ -1,11 +1,11 @@
-resource "digitalocean_domain" "default" {
+resource "digitalocean_domain" "erisfairbanks" {
   name       = "erisfairbanks.com"
-  ip_address = digitalocean_droplet.stream.ipv4_address
+  ip_address = digitalocean_droplet.erisfairbanks.ipv4_address
 }
 
-resource "digitalocean_droplet" "stream" {
+resource "digitalocean_droplet" "erisfairbanks" {
   image = "ubuntu-18-04-x64"
-  name = "dobox"
+  name = "erisfairbanks"
   region = "nyc1"
   size = "s-1vcpu-1gb"
   private_networking = true
@@ -35,14 +35,48 @@ resource "digitalocean_droplet" "stream" {
   }
 }
 
-resource "digitalocean_domain" "ospedge" {
-  name       = "ospedge.erisfairbanks.com"
-  ip_address = digitalocean_droplet.ospedge2.ipv4_address
+resource "digitalocean_domain" "cybox" {
+  name       = "cybox.erisfairbanks.com"
+  ip_address = digitalocean_droplet.cybox.ipv4_address
 }
 
-resource "digitalocean_droplet" "ospedge2" {
+resource "digitalocean_droplet" "cybox" {
   image = "ubuntu-18-04-x64"
-  name = "dobox"
+  name = "cybox"
+  region = "nyc1"
+  size = "s-1vcpu-1gb"
+  private_networking = true
+  ssh_keys = [
+    data.digitalocean_ssh_key.terraform.id
+  ]
+  connection {
+    host = self.ipv4_address
+    user = "root"
+    type = "ssh"
+    private_key = file(var.pvt_key)
+    timeout = "2m"
+  }
+  provisioner "remote-exec" {
+    inline = [
+      "sudo apt update",
+      "sudo apt install -y apt-transport-https ca-certificates curl software-properties-common",
+      "curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -",
+      "sudo add-apt-repository 'deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable'",
+      "sudo apt update",
+      "apt-cache policy docker-ce",
+      "sudo apt install -y docker-ce"
+    ]
+  }
+}
+
+resource "digitalocean_domain" "ospedge" {
+  name       = "ospedge.erisfairbanks.com"
+  ip_address = digitalocean_droplet.ospedge.ipv4_address
+}
+
+resource "digitalocean_droplet" "ospedge" {
+  image = "ubuntu-18-04-x64"
+  name = "ospedge"
   region = "nyc1"
   size = "s-2vcpu-2gb"
   private_networking = true
@@ -64,7 +98,7 @@ resource "digitalocean_droplet" "ospedge2" {
       "rm setup-ospEdge.sh",
       "wget https://raw.githubusercontent.com/efairbanks/dobox/master/setup-ospEdge.sh",
       "chmod +x setup-ospEdge.sh",
-      "sudo bash setup-ospEdge.sh <<< 'edge1.commie.cafe'"
+      "sudo bash setup-ospEdge.sh <<< '70.64.32.228'"
       /*
       "cp setup/nginx/servers/osp-servers.conf .",
       "cp setup/nginx/services/osp-rtmp.conf .",
